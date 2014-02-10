9e3b99a33f63a4ca5446ec03991062e50ab733e7
chrissyzic <chrissy.ziccarelli@gmail.com>
2014-02-09 19:50:48 -0500

    Update pbjtime.py

--- pbjtime.py
+++ pbjtime.py
@@ -1,63 +1,55 @@
-pb = 6
-jelly = 6
+pb = 1
+jelly = 0
 slices = 11
 
 #First Goal
-if pb >= 1 and jelly >= 1 and slices >=2:
+if pb and jelly and slices/2 >= 1:
     print "You can make a sandwich!"
 else:
     print "No PB&J for you :("
 
 #Second Goal
-if pb >= 1 and jelly >= 1 and slices >=2:
-    print "You can make {0} sandwiches!".format(min(pj,jelly,slices/2))
+if pb and jelly and slices/2 >= 1:
+    print "You can make {0} sandwiches!".format(min(pb,jelly,slices/2))
 else:
     print "No PB&J for you :("
 
 #Third Goal
-#If you have an odd number of bread slices, you can only make an open faced sandwich if you have enough pb & j for the extra slice of bread jelly = (slices/2)+1
-if pb == 0 or jelly == 0 or slices == 0:
-    print "No PB&J for you :("
-elif jelly <= pb and jelly <= slices/2 and slices % 2 == 0:
-    print "You can make {0} sandwiches!".format(jelly)
-elif jelly <= pb and jelly <= slices/2 and slices % 2 == 1:
-    print "You can make {0} sandwiches!".format(jelly)
-elif pb <= jelly and pb <= slices/2 and slices % 2 == 0:
-    print "You can make {0} sandwiches!".format(pb)
-elif pb <= jelly and pb <= slices/2 and slices % 2 == 1:
-    print "You can make {0} sandwiches!".format(pb)
-elif slices/2 <= pb and slices/2 <= jelly and slices % 2 == 0:
-    print "You can make {0} sandwiches!".format(slices/2)
-elif slices/2 <= pb and slices/2 <= jelly and slices % 2 == 1:
-    print "You can make {0} full sandwiches and an open faced one!".format(slices/2)
+if pb and jelly and slices/2 >= 1 and slices % 2 == 0:
+    print "You can make {0} sandwiches!".format(min(pb,jelly,slices/2))
+elif slices % 2 == 1 and jelly > slices/2 and pb > slices/2:
+    print "You can make {0} sandwiches and an open faced one.".format(min(pb,jelly,slices/2))
+elif slices % 2 == 1 and jelly and pb >= 1:
+    print "You can make {0} sandwiches.".format(min(pb,jelly,slices/2))
 else:
     print "No PB&J for you :("
-
-    """
-Another way to approach Third Goal
-if pb or jelly or slices == 0:
-    print "No PB&J for you :("
-elif jelly or pb or slices/2 >= 1:
-    print "You can make {0} sandwiches!".formt(min(jelly,pb,slices/2))"""
 
 #Fourth Goal
-if pb == 0:
-    print "You need some peanut butter."
-elif jelly == 0:
-    print "You need some jelly."
-elif slices == 0:
-    print "You need some bread!"
-elif jelly <= pb and jelly <= slices/2 and slices % 2 == 0:
-    print "You can make {0} sandwiches!".format(jelly)
-elif jelly <= pb and jelly <= slices/2 and slices % 2 == 1:
-    print "You can make {0} sandwiches!".format(jelly)
-elif pb <= jelly and pb <= slices/2 and slices % 2 == 0:
-    print "You can make {0} sandwiches!".format(pb)
-elif pb <= jelly and pb <= slices/2 and slices % 2 == 1:
-    print "You can make {0} sandwiches!".format(pb)
-elif slices/2 <= pb and slices/2 <= jelly and slices % 2 == 0:
-    print "You can make {0} sandwiches!".format(slices/2)
-elif slices/2 <= pb and slices/2 <= jelly and slices % 2 == 1:
-    print "You can make {0} full sandwiches and an open faced one!".format(slices/2)
+if pb and jelly and slices/2 >= 1 and slices % 2 == 0:
+    print "You can make {0} sandwiches!".format(min(pb,jelly,slices/2))
+elif slices % 2 == 1 and jelly > slices/2 and pb > slices/2:
+    print "You can make {0} sandwiches and an open faced one.".format(min(pb,jelly,slices/2))
+elif slices % 2 == 1 and jelly and pb >= 1:
+    print "You can make {0} sandwiches.".format(min(pb,jelly,slices/2))
 else:
-    print "No PB&J for you :("
+    if pb == 0:
+        print "You need peanut butter."
+    if jelly == 0:
+        print "You need jelly."
+    if slices == 0:
+        print "You need bread."
+
+#Fifth Goal
+if pb and jelly and slices/2 >= 1 and slices % 2 == 0:
+    print "You can make {0} sandwiches!".format(min(pb,jelly,slices/2))
+elif slices % 2 == 1 and jelly > slices/2 and pb > slices/2:
+    print "You can make {0} sandwiches and an open faced one.".format(min(pb,jelly,slices/2))
+elif slices % 2 == 1 and jelly and pb >= 1:
+    print "You can make {0} sandwiches.".format(min(pb,jelly,slices/2))
+elif pb and slices/2 >=1 and jelly == 0:
+    print "You can make a peanut butter sandwich, but what kind of person would do that? Gross."
+else:
+    if pb == 0:
+        print "You need peanut butter."
+    if slices == 0:
+        print "You need bread."
