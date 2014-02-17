97208431ff15d57f16614c9ebafe4bde3f6c76a9
chrissyzic <chrissy.ziccarelli@gmail.com>
2014-02-17 18:01:30 -0500

    Create fizzbuzz.py

---
+++ fizzbuzz.py
@@ -0,0 +1,16 @@
+# Goal 1: FizzBuzz Classic
+
+# Write a program that prints the numbers from 1 to 100.
+# But for multiples of three print "Fizz" instead of the number.
+# For the multiples of five print "Buzz."
+# For numbers which are multiples of both three and five print "FizzBuzz."
+
+for number in range (1, 101, 1):
+    if number % 3 == 0 and number % 5 == 0:
+        print "FizzBuzz"
+    elif number % 3 == 0:
+        print "Fizz"
+    elif number % 5 == 0:
+        print "Buzz"
+    else:
+        print "{0}".format(number)
