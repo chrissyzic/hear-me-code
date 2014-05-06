791adbc27c141ad1b70904c54428f677c60ee45c
chrissyzic <chrissy.ziccarelli@gmail.com>
2014-05-05 23:40:28 -0400

    Update lesson03_playtime_compare.py

--- lesson03_playtime_compare.py
+++ lesson03_playtime_compare.py
@@ -12,20 +12,34 @@
 # Challenge 1: Open all_employees.csv and survey.csv and compare the two.  When an employee from survey.csv appears in all_employees.csv, print out the rest of their information from all_employees.csv.
 
 with open ("all_employees.csv","r") as all_employees_file:
-	employees = all_employees_file.read().split(",")
+	employees = all_employees_file.read().split("\n")
 
 for index, employee in enumerate(employees):
-    employees[index] = employee.split("\n")
+    employees[index] = employee.split(",")
+    names = str(employees[index][0])
+    email = str(employees[index][1])
+    phone = str(employees[index][2])
+    department = str(employees[index][3])
+    position = str(employees[index][4])
+    #print names, phone, email, department, position
 
 with open ("survey.csv","r") as survey_file:
-	responses = survey_file.read()
+    responders = survey_file.read().split("\n")
 
-print employees
-#print responses
+for index, responder in enumerate(responders):
+    responders[index]= responder.split(",")
+    email_survey = str(responders[index][0])
+    twitter = str(responders[index][1])
+    github = str(responders[index][2])
 
+if email == email_survey:
+    print twitter
+    
+# for index, responder in enumerate(responders):
 # Sample output:
 #       Shannon Turner took the survey! Here is her contact information: Twitter: @svt827 Github: @shannonturner Phone: 202-555-1234
 
 # Challenge 2: Add the extra information from survey.csv into all_employees.csv as extra columns.  
 # IMPORTANT: It would probably be a good idea to save it as an extra file instead of accidentally overwriting your original!
 # By the end, your all_employees.csv should contain the following columns: name, email, phone, department, position, twitter, github
+
